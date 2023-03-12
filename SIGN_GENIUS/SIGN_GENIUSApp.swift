54dc095e246ce0d9851e16474f311593e60eaeb6
Antonio Espino Muñoz <antonioespinom@gmail.com>
2023-03-12 12:45:54 +0100

    Initial Commit

---
+++ SIGN_GENIUS/SIGN_GENIUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIGN_GENIUSApp.swift
+//  SIGN_GENIUS
+//
+//  Created by Antonio Espino Muñoz on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SIGN_GENIUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
